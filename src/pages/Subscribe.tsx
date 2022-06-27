a3dea654f1b6f283456cad03d2b663b06e23328a
Diego Czajka <diego.czajka@gmail.com>
2022-06-26 21:42:10 -0300

    bug/fix

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -9,7 +9,7 @@ export function Subscribe() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
 
-  const [createSubscriber] = useCreateSubscriberMutation();
+  const [createSubscriber, { loading }] = useCreateSubscriberMutation();
 
   async function handleSubscribe(event: FormEvent) {
     event.preventDefault();
@@ -26,14 +26,14 @@ export function Subscribe() {
 
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className=" w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
         <div className="max-w-[640px]">
           <Logo />
 
           <h1 className="mt-8 text-[2.5rem] leading-tight">
-            Consutra uma{" "}
-            <strong className="text-blue-500"> aplicação completa </strong>, do
-            zero, <strong className="text-blue-500">React</strong>
+            Construa uma{" "}
+            <strong className="text-blue-500">aplicação completa</strong>, do
+            zero, com <strong className="text-blue-500">React JS</strong>
           </h1>
           <p className="mt-4 text-gray-200 leading-relaxed">
             Em apenas uma semana você vai dominar na prática uma das tecnologias
@@ -41,30 +41,33 @@ export function Subscribe() {
             oportunidades do mercado.
           </p>
         </div>
+
         <div className="p-8 bg-gray-700 border border-gray-500 rounded">
           <strong className="text-2xl mb-6 block">
             Inscreva-se gratuitamente
           </strong>
+
           <form
             onSubmit={handleSubscribe}
             className="flex flex-col gap-2 w-full"
           >
             <input
-              type="text"
               className="bg-gray-900 rounded px-5 h-14"
-              placeholder="Seu Nome Completo"
+              type="text"
+              placeholder="Seu nome completo"
               onChange={(event) => setName(event.target.value)}
             />
             <input
-              type="text"
               className="bg-gray-900 rounded px-5 h-14"
+              type="email"
               placeholder="Digite seu e-mail"
               onChange={(event) => setEmail(event.target.value)}
             />
 
             <button
               type="submit"
-              className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors"
+              disabled={loading}
+              className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
             >
               Garantir minha vaga
             </button>
@@ -72,7 +75,7 @@ export function Subscribe() {
         </div>
       </div>
 
-      <img src="/src/assets/mockup.png" className="mt-10" alt="" />
+      <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
     </div>
   );
 }
